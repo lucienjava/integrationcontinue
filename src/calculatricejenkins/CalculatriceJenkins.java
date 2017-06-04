383673964ecb0283b80dbc85af6ce026ff9622ac
lucien <lucien@PC-Lucien>
2017-06-04 15:46:00 +0200

    Nouveau build
    test declenchement auto

---
+++ src/calculatricejenkins/CalculatriceJenkins.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatricejenkins;
+
+/**
+ *
+ * @author lucien
+ */
+public class CalculatriceJenkins {
+
+    /**
+     * Additionne deux réels passés en parametre
+     * @param a de type double 
+     * @param  b de type double
+     * @return la somme des deux parametres
+     */
+    public double addition(double a, double b){
+        return a+b;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
